046adeccf0f6ae0dcc147d3875481b63cee924a5
정규영 <jgy_98@naver.com>
2024-05-19 14:52:25 +0900

    feat : header, footer 만들어서 layout에 적용

---
+++ src/app/common/layout/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from "../style/footer";
+
+const Footer = () => {
+    return (
+        <>
+            <S.Footer>
+                <S.Title>Maple Land</S.Title>
+            </S.Footer>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/app/common/layout/components/header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from "../../assets/img/mapleLogo.png";
+import Image from "next/image";
+import * as S from "../style/header";
+
+const Header = () => {
+    const headerList = ["직업", "아이템", "이벤트"];
+    
+    return (
+        <>
+            <S.Header>
+                <Image src={Logo} width={60} alt='Logo'/>
+                <S.ItemList>
+                    {
+                        headerList.map((list, index) => {
+                            return (
+                                <S.List key={index}>{list}</S.List>
+                            )
+                        })
+                    }
+                </S.ItemList>
+            </S.Header>
+        </>
+    );
+};
+
+export default Header;
