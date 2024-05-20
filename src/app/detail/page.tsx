74820e7ae7f5a5e4e6ef0d5d8fcbd0164dfcd02e
정규영 <jgy_98@naver.com>
2024-05-20 22:04:08 +0900

    feat : /detail 추가

---
+++ src/app/detail/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UseItem from "./components/userItem";
+
+const Detail = () => {
+    return (
+        <div>
+            <UseItem />
+            detail
+        </div>
+    );
+};
+
+export default Detail;
