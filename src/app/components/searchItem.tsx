99d970feb0e6c3b9bbc6db1ed5c213d0b79be60d
정규영 <jgy_98@naver.com>
2024-05-20 21:59:24 +0900

    feat : 검색 결과 반한하는 UI 추가

---
+++ src/app/components/searchItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as S from "../style/searchItem";
+
+interface ISearchItem {
+    item: any;
+}
+
+const SearchItem = ({item}: ISearchItem) => {
+    return (
+        <>
+            <S.SearchItem>
+                {item.level}
+            </S.SearchItem>
+        </>
+    );
+};
+
+export default SearchItem;
