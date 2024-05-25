180bd9026ee7645bc6c01ba97967bacef8d23690
정규영 <jgy_98@naver.com>
2024-05-25 18:01:43 +0900

    chore : 검색 후 유저 정보 나타내는 컴포넌트 생성

---
+++ src/app/detail/components/userItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from "../style/userItem";
+
+const UserItem = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default UserItem;
