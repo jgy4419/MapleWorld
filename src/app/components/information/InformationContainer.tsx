af554c5c98c33462be6dc13746fa15bce7a9dca0
정규영 <jgy_98@naver.com>
2024-05-25 17:56:22 +0900

    design : 유저 검색 전 나타나는 UI 생성

---
+++ src/app/components/information/InformationContainer.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React, { useState } from 'react';
+import * as S from "./style/informationContainer";
+import Ranking from "./Ranking";
+import { MainInformationUl } from '@/util/constants';
+
+const InformationContainer = () => {
+    const [clickItem, setClickItem] = useState(MainInformationUl[0]);
+    return (
+        <>
+            <S.Container>
+                <S.Inner>
+                    <S.MainInformationList>
+                        {
+                            MainInformationUl.map((list, index) => {
+                                return (
+                                    <S.ListItem onClick={() => {setClickItem(list)}} key={index}>{ list }</S.ListItem>           
+                                )
+                            })
+                        }
+                    </S.MainInformationList>
+                    <Ranking clickItem={clickItem}/>
+                </S.Inner>
+            </S.Container>
+        </>
+    );
+};
+
+export default InformationContainer;
