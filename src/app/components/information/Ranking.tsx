1d7d179defefa6b97b2b05002e216062183762bc
정규영 <jgy_98@naver.com>
2024-05-27 21:54:55 +0900

    design : 유저 랭킹, 길드 랭킹 불러오는 컴포넌트 분리

--- src/app/components/information/Ranking.tsx
+++ src/app/components/information/Ranking.tsx
@@ -1,5 +1,24 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import * as S from "./style/Ranking";
+import UserRanking from './UserRanking';
+import GuildRanking from './GuildRanking';
+
+interface IRanking {
+    character_name: string,
+    character_exp: number,
+    character_guildname: string,
+    character_level: number,
+    character_popularity: number,
+    class_name: string,
+    date: string,
+    ranking: number,
+    sub_class_name: string,
+    world_name: string
+}
+
+const Ranking = ({ clickItem }: any) => {
+    const [userList, setUserList] = useState<IRanking[]>([]);
 
     const fetchUserRanking = async () => {
             // 유저 랭킹인지 길드 랭킹인지 조건 처리하기
@@ -41,9 +60,22 @@ import React, { useEffect, useState } from 'react';
 
     return (
         <>
-            {userList.ranking[0].date}
+            {/* 
+                [TODO]
+                1. 무한스크롤 구현하기 
+                2. 캐싱하기
+            */}
+            <S.RankingList>
+                {
+                    clickItem === "종합 랭킹"
+                    ? <UserRanking userList={userList} />
+                    : <GuildRanking /> 
+                }
+            </S.RankingList>
         </>
     );
 };
+
+
 
 export default Ranking;
