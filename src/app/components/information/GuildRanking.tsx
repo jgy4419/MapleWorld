8cf2d572abcb69f0121a5b8737496d1bc93e72ed
정규영 <jgy_98@naver.com>
2024-05-27 21:52:55 +0900

    design : 길드 랭킹 가져오는 UI 뼈대 생성

---
+++ src/app/components/information/GuildRanking.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from "./style/GuildRanking";
+
+const GuildRanking = () => {
+    return (
+        <h1>
+            길드 랭킹
+        </h1>
+    );
+};
+
+export default GuildRanking;
