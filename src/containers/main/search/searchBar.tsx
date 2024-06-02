cf879611ccb5d9ee1fe15bb8ecdfc729bc0faebd
정규영 <jgy_98@naver.com>
2024-06-02 16:59:48 +0900

    feat : 검색 버튼 클릭 시 검색 결과 store에 전송

--- src/containers/main/search/searchBar.tsx
+++ src/containers/main/search/searchBar.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
 import * as S from "./style/searchBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector } from "react-redux";
+import { searchResult } from "@/store/searchValue";
+import { RootState } from "@/store/rootReducer";
 
 const SearchBar = () => {
+    const dispatch = useDispatch();
+    const value = useSelector((state: RootState) => state.searchRes);
+    const searchInputElement = useRef<HTMLInputElement>(null);
+
+    const searchEvent = async () => {  // TypeScript의 타입을 맞추기 위해 변경
+        if(searchInputElement.current) {
+            dispatch(searchResult(searchInputElement.current.value));
+            console.log(value);
+        }
+    }
+
     return (
         <>
             <S.SearchContain>
-                <S.SearchInput placeholder='유저 닉네임을 검색해주세요!'/>
-                <S.SearchButton>
+                <S.SearchInput ref={searchInputElement} placeholder='유저 닉네임을 검색해주세요!'/>
+                <S.SearchButton onClick={searchEvent}>
                     <FontAwesomeIcon width={30} icon={faMagnifyingGlass} color="#AF8F6F"/>
                 </S.SearchButton>
             </S.SearchContain>
