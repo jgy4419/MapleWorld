df69887a076be738292e22e3b4317d39b6bd37b6
정규영 <jgy_98@naver.com>
2024-06-03 21:14:16 +0900

    feat : 현재 날짜 불러오는 함수 common.ts 에 생성 후 ranking api 에 적용

--- src/app/api/nexon/ranking/userRanking/route.ts
+++ src/app/api/nexon/ranking/userRanking/route.ts
@@ -1,7 +1,9 @@
 import { fetchGet } from "@/services/get";
+import { getToday } from "@/constants/common";
 
 export async function GET() {
-    const [date] = ["2024-05-25"];
+    // 항상 최신 날짜로 수정
+    const date = getToday();
     const urlString = `ranking/overall?date=${date}`;
 
     return fetchGet(urlString);
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,1 +1,11 @@
 export const maple_api_url = "https://open.api.nexon.com/maplestory/v1/";
+
+export const getToday = () => {
+    let today = new Date();
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
+}
