096437d03a5a8a306d21a3c33c55c5258e5b824f
정규영 <jgy_98@naver.com>
2024-06-03 21:13:03 +0900

    update : 검색 결과에 따라 보여질 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
+'use client';
 import React from 'react';
 import * as S from "@/containers/main/Main";
 import EventImage from "@/containers/main/information/EventImage";
 import SearchBar from "@/containers/main/search/searchBar";
 import Information from "@/containers/main/information/InformationContainer";
 import SearchResult from "@/containers/main/search/searchResult";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/rootReducer";
+
 
 const Page = () => {
+  const value = useSelector((state: RootState) => state.searchRes).search;
+  
   return (
     <>
       <S.MainContainer>
         <S.Inner>
           <EventImage />
           <SearchBar />
-          <Information />
-          {/* <SearchResult /> */}
+          {
+            value === ""
+              ? <Information />
+              : <SearchResult searchResult={value}/>
+          }
         </S.Inner>
         </S.MainContainer>
     </>
