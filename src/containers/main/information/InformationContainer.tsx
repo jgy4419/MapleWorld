47dc5f60c7d6a1f5395c96f8ea5ac414f05db9eb
정규영 <jgy_98@naver.com>
2024-06-16 07:57:57 +0900

    update : 스크롤 로직 hooks로 변경 시키고, 적용

--- src/containers/main/information/InformationContainer.tsx
+++ src/containers/main/information/InformationContainer.tsx
@@ -1,20 +1,47 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as S from "./style/informationContainer";
 import Ranking from "./Ranking";
 import { MainInformationUl } from '@/constants/main';
+import { selectTitleChange } from '@/store/searchValue';
+import { useDispatch } from 'react-redux';
+import useGetScrollData from './hook/useScrollData';
 
 const InformationContainer = () => {
+    const dispatch = useDispatch();
     const [clickItem, setClickItem] = useState(MainInformationUl[0]);
+    const rankingContainer = useRef<HTMLDivElement>(null);
+    const { handleScroll } = useGetScrollData(rankingContainer);
+
+    useEffect(() => {
+        const rankingContainerDom = rankingContainer.current;
+        if(rankingContainerDom) {
+            rankingContainerDom.addEventListener("scroll", handleScroll);
+
+            return () => {
+                rankingContainerDom.removeEventListener("scroll", handleScroll);
+            }
+        }
+    }, [rankingContainer, clickItem]);
+
+    const clickCategory = (list: string) => {
+        dispatch(selectTitleChange(
+            list === "종합 랭킹"
+                ? "user"
+                : "guild"
+        ));
+        setClickItem(list);
+    }
+
     return (
         <>
-            <S.Container>
+            <S.Container ref={rankingContainer}>
                 <S.Inner>
                     <S.MainInformationList>
                         {
                             MainInformationUl.map((list, index) => {
                                 return (
-                                    <S.ListItem onClick={() => {setClickItem(list)}} key={index}>{ list }</S.ListItem>           
+                                    <S.ListItem onClick={() => {clickCategory(list)}} key={index}>{ list }</S.ListItem>           
                                 )
                             })
                         }
