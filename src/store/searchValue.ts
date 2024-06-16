562e44c6d3396f0ac012041631e8a576fff0a434
정규영 <jgy_98@naver.com>
2024-06-16 09:22:29 +0900

    update : store에 채널 이름, 리스트아이디, 유저, 길드 리스트 개수 담는 로직 추가

--- src/store/searchValue.ts
+++ src/store/searchValue.ts
@@ -1,14 +1,21 @@
-// src/store/searchSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface SearchState {
     search: string;
     selectTitle: string;
+    worldName: string;
+    listContainerId: string;
+    userListLength: number,
+    guildListLength: number
 }
   
   const initialState: SearchState = {
     search: "",
     selectTitle: "user",
+    worldName: "",
+    listContainerId: "",
+    userListLength: 0,
+    guildListLength: 0
   };
 
 const searchValue = createSlice({
@@ -16,13 +23,32 @@ const searchValue = createSlice({
   initialState,
   reducers: {
     searchResult: (state, action: PayloadAction<string>) => {
-        state.search = action.payload;
+      state.search = action.payload;
     },
     selectTitleChange: (state, action: PayloadAction<string>) => {
-        state.selectTitle = action.payload
+      state.selectTitle = action.payload
+    },
+    worldNameChange: (state, action: PayloadAction<string>) => {
+      state.worldName = action.payload;
+    },
+    listContainerChange: (state, action: PayloadAction<any>) => {
+      state.listContainerId = action.payload;
+    },
+    userListLengthChange: (state) => {
+      state.userListLength += 20;
+    },
+    guildListLengthChange: (state) => {
+      state.guildListLength += 20;
     }
   },
 });
 
-export const { searchResult, selectTitleChange } = searchValue.actions;
+export const { 
+  searchResult, 
+  selectTitleChange,
+  worldNameChange, 
+  listContainerChange,
+  userListLengthChange,
+  guildListLengthChange
+ } = searchValue.actions;
 export default searchValue.reducer;
