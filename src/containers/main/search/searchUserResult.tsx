582fa560d8035343d16f95762ee2e6c1422a7aea
정규영 <jgy_98@naver.com>
2024-06-19 21:57:31 +0900

    fix : 상세정보 확인 시 에러 발생하는 문제 수정

--- src/containers/main/search/searchUserResult.tsx
+++ src/containers/main/search/searchUserResult.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from "./style/searchUserResult";
 import { errorName } from '@/constants/main';
 import NotResult from '@/app/common/layout/components/notResult';
+import Image from "next/image";
 
 const SearchUserResult = ({ userData, searchResult }: any) => {
+    useEffect(() => {
+        console.log(userData.character_image);
+    }, [userData.character_image]);
     return (
         <>
             {
                 userData !== errorName.notUser
                     ? <>
-                        <S.CharacterImage url={userData.character_image} />
+                        {userData.character_image && <Image src={userData.character_image} width={150} height={150} alt="검색 결과"/>}
                         <S.CharacterInfo>
                             <S.CharacterName>이름 : {searchResult}</S.CharacterName>
                             <S.CharacterClass>직업 : {userData.character_class}</S.CharacterClass>
