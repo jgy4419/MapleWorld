e78c43e7980e410178a2dccd35143b106d598fba
정규영 <jgy_98@naver.com>
2024-06-20 21:47:27 +0900

    update : favorite 컴포넌트 수정

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -1,26 +1,35 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Star from "@/app/common/assets/img/Star.png"
 import NonStar from "@/app/common/assets/img/NonStar.png"
+import { FavoriteSetting } from '@/utils/FavoriteSetting';
 
 interface IFavorite {
+    title: string;
     state: boolean;
 }
 
-const Favorite = ({ state }: IFavorite) => {
+const Favorite = ({ title, state }: IFavorite) => {
+    const [starState, setStarState] = useState(state);
+
+    const buttonClickHandler = () => {
+        FavoriteSetting(title, state);
+        setStarState(prev => !prev);
+    }
+
     return (
         <>
-            <Container>
+            <Container onClick={() => buttonClickHandler()}>
                 <Title>
                 {
-                    state === true  
+                    starState === true  
                         ? "Started"
                         : "Star"
                 }
                 </Title>
                 {
-                    state === true  
+                    starState === true  
                         ? Star && <Image src={Star} width={30} height={30} alt="star"/>
                         : NonStar && <Image src={NonStar} width={30} height={30} alt="non-star"/>
                 }
@@ -30,11 +39,14 @@ const Favorite = ({ state }: IFavorite) => {
 };
 
 const Container = styled.div`
+    position: relative;
+    z-index: 1000;
     width: 150px;
     height: 50px;
     display: flex;
     align-items: center;
     justify-content: space-around;
+    cursor: pointer;
 `;
 
 const Title = styled.h4`
