c522c53176681629b17fff6a072df24a849cbbfd
정규영 <jgy_98@naver.com>
2024-06-23 15:43:50 +0900

    update : 유저, 길드 랭킹 순위 나타내는 컴포넌트 따로 생성해서 관리했었는데, 하나의 컴폰너트로 통합

--- src/containers/main/information/style/Ranking.ts
+++ src/containers/main/information/style/Ranking.ts
@@ -8,3 +8,43 @@ export const RankingList = styled.ul`
     border-radius: 20px;
     margin: auto;
 `;
+
+export const RankingItem = styled.li`
+    display: flex;    
+    gap: 20px;
+    width: 100%;
+    height: 130px;
+    border-radius: 20px;
+    margin-left: -20px;
+    padding: 20px;
+    box-sizing: border-box;
+    margin-top: 20px;
+    background: #F6E6CB;
+    font-weight: 600;
+`;
+
+export const Count = styled.span`
+    font-size: 20px;
+`;
+
+export const ItemInner = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 3px;
+    span {
+        width: 100%;
+    }
+`;
+
+export const Name = styled.span`
+`;
+
+export const Level = styled.span`
+
+`;
+
+export const WorldName = styled.span`
+`;
+
+export const Etc = styled.span`
+`;
