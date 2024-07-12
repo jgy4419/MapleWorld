eeec547848df4972b57f9df5b58cda2d1cc381bb
정규영 <jgy_98@naver.com>
2024-07-12 12:47:48 +0900

    update : loading ui 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const Loading = () => {
     return (
-        <LoadingContain>
-            Loading...
-        </LoadingContain>
+        <>
+            <span className="loader" />
+            <style jsx>{`
+                .loader {
+                    width: 70px;
+                    height: 70px;
+                    display: inline-block;
+                    position: relative;
+                }
+                .loader::after,
+                .loader::before {
+                    content: '';  
+                    box-sizing: border-box;
+                    width: 70px;
+                    height: 70px;
+                    border-radius: 50%;
+                    border: 3px solid #333;
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    animation: animloader 2s linear infinite;
+                }
+                .loader::after {
+                    animation-delay: 1s;
+                }
+
+                @keyframes animloader {
+                    0% {
+                        transform: scale(0);
+                        opacity: 1;
+                    }
+                    100% {
+                        transform: scale(1);
+                        opacity: 0;
+                    }
+                }
+            `}
+            </style>
+        </>
     );
 };
 
 export default Loading;
-
-const LoadingContain = styled.div`
-    position: relative;
-    font-size: 100px;  
-    color: #fff;
-`;
